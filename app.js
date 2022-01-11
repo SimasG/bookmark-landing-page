35cb569c7afdb2c0f17b3d26c5b6bd5b0c2a0cdb
simutis <simhka15@gmail.com>
2022-01-12 00:46:03 +0200

    progressed on the features section

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navUl = document.querySelector(".nav-ul");
+// For later
+const featureList = document.querySelector(".feature-list");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navUl.classList.toggle("active");
 });
+
 
 
