ad204536926ee20ea0ae7d15bec8520a66d90269
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-13 18:29:29 +0200

    modularized the css

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const navUl = document.querySelector(".nav-ul-styling");
+const logo = document.querySelector(".logo")
+
 // For later maybe
 const featureList = document.querySelector(".feature-list");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navUl.classList.toggle("active");
+
+    // Logo color toggle
+    if (navUl.classList.contains("active")){
+        logo.src = "images/logo-bookmark-white.svg";
+    }else{
+        logo.src = "images/logo-bookmark.svg";
+    }
 });
 
 
