382a22c9361c105336ebeacb8e93ed5957e2c3ce
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-14 00:30:26 +0200

    progressed on building a feature tab functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const navUl = document.querySelector(".nav-ul-styling");
 const logo = document.querySelector(".logo")
 const socialMediaIcon = document.querySelector(".social-media-icon-container-header");
 
+const tabs = document.querySelectorAll("[data-tab-target");
+
 // For later maybe
 const featureList = document.querySelector(".feature-list");
 
@@ -18,6 +20,14 @@ hamburger.addEventListener("click", () => {
         logo.src = "images/logo-bookmark.svg";
     }
 });
+
+// Loop for Tabs
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        // Don't understand how are "dataset" and "tabTarget" related to anything
+        const target = document.querySelector(tab.dataset.tabTarget)
+    })
+})
 
 
 
