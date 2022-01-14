a56f2154019907ef3a9f0eb903c235cfcb03a8fd
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-14 20:39:27 +0200

    80% done with the feature tab functionality

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const navUl = document.querySelector(".nav-ul-styling");
 const logo = document.querySelector(".logo")
 const socialMediaIcon = document.querySelector(".social-media-icon-container-header");
 
-const tabs = document.querySelectorAll("[data-tab-target");
-
-// For later maybe
-const featureList = document.querySelector(".feature-list");
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+const tabsUnderline = document.querySelectorAll("[data-tab-underline]");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -24,8 +23,23 @@ hamburger.addEventListener("click", () => {
 // Loop for Tabs
 tabs.forEach(tab => {
     tab.addEventListener("click", () => {
-        // Don't understand how are "dataset" and "tabTarget" related to anything
+        // Don't understand how are "dataset" and "tabTarget" related to anything.
+        // "dataset" is a set property used to get the data attribute in HTML
+        // "tabTarget" is a camelCase version of "tab-target" in HTML
         const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => { 
+            tabContent.classList.remove("active")
+        })
+        tabs.forEach(tab => { 
+            tab.classList.remove("active")
+        })
+        // tabsUnderline.forEach(tabUnderline => {
+        //     tabUnderline.classList.remove("active")
+        // })
+
+        // tabUnderline.classList.add("active")
+        tab.classList.add("active")
+        target.classList.add("active")
     })
 })
 
