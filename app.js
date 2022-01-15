38b6d07fb6211282b146adb2d14cd7115d577da2
simutis <54304022+SimasG@users.noreply.github.com>
2022-01-15 14:05:39 +0200

    finished faq section functionality

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
+// Header
 const hamburger = document.querySelector(".hamburger");
 const navUl = document.querySelector(".nav-ul-styling");
 const logo = document.querySelector(".logo")
 const socialMediaIcon = document.querySelector(".social-media-icon-container-header");
-
-// const tabs = document.querySelectorAll("[data-tab-target]" + "[data-tab-underline]");
-const tabs = document.querySelectorAll("[data-tab-target], [data-tab-underline]");
-const tabContents = document.querySelectorAll("[data-tab-content]");
-const tabsUnderline = document.querySelectorAll("[data-tab-underline-child]");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navUl.classList.toggle("active");
     socialMediaIcon.classList.toggle("active");
 
-    // Logo color toggle
     if (navUl.classList.contains("active")){
         logo.src = "images/logo-bookmark-white.svg";
     }else{
@@ -21,12 +16,19 @@ hamburger.addEventListener("click", () => {
     }
 });
 
-// Loop for Tabs
+// Tabs
+const tabs = document.querySelectorAll("[data-tab-target], [data-tab-underline]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+const tabsUnderline = document.querySelectorAll("[data-tab-underline-child]");
+
 tabs.forEach(tab => {
     tab.addEventListener("click", () => {
         // Don't understand how are "dataset" and "tabTarget" related to anything.
         // "dataset" is a set property used to get the data attribute in HTML
         // "tabTarget" is a camelCase version of "tab-target" in HTML
+
+        // These lines are supposed to connect tab-target with tab-content and tab-underline with tab-underline-child.
+        // I don't understand the connection though.
         const target = document.querySelector(tab.dataset.tabTarget)
         const underline = document.querySelector(tab.dataset.tabUnderline)
 
@@ -42,25 +44,35 @@ tabs.forEach(tab => {
             tab.classList.remove("active")
         })
 
-        // ATTEMPT 1
-        // const underline = document.querySelector(tab.dataset.tabUnderline)
-        // tabsUnderline.forEach(tabUnderline => {
-        //     tabUnderline.classList.remove("active")
-        // })
-        // underline.classList.add("active")
-        
-        // ATTEMPT 2
-        // tabsUnderline.forEach(tabUnderline => {
-        //     tabUnderline.classList.remove("active")
-        // })
-
-        // tabUnderline.classList.add("active")
-
-
-        // TRY AGAIN TOMORROW
-
         tab.classList.add("active")
         target.classList.add("active")
         underline.classList.add("active")
+    })
+})
+
+// FAQs -> the same as Tabs. Only difference - if clicked again, the arrow should turn back and the faq answer removed
+const faqs = document.querySelectorAll("[data-faq-target], [data-faq-arrow]");
+const faqContents = document.querySelectorAll("[data-faq-content]");
+const faqArrows = document.querySelectorAll("[data-faq-arrow-child]");
+
+
+// All faq arrows
+faqs.forEach(faq => {
+    // Listen for the "click" for each far arrow
+    faq.addEventListener("click", () => {
+        // This line is supposed to connect the questions (faq-target) with answers (faq-content).
+        // I don't understand the connection though.
+        const faqTarget = document.querySelector(faq.dataset.faqTarget)
+        // const arrow = document.getElementById("arrow")
+
+        faqTarget.classList.toggle("active")
+
+        // Making sure that all other arrows are "closed" once we click on a new faq
+        const arrow = document.querySelector(faq.dataset.faqArrow)
+
+        // faqArrows.forEach(faqArrow => {
+        //     faqArrow.classList.remove("active")
+        // })
+        arrow.classList.toggle("active")
     })
 })
